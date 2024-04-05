3cb4e17775447096a3c244eb9247993c899014de
Patricio Cordero <patricio@espejozen.cl>
2024-04-05 17:56:34 -0300

    agrega keymaps

--- nvim/lua/evone2024/core/keymaps.lua
+++ nvim/lua/evone2024/core/keymaps.lua
@@ -30,3 +30,9 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current" })
+keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close current" })
+keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Save and close current" })
+
+keymap.set("n", "<leader>a", "<C-w><C-w><CR>", { desc = "Save current" })
